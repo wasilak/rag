e2e3e1d73caf4619172732ea441c757603339bc7
Piotr Boruc <piotr.m.boruc@gmail.com>
2025-07-31 21:22:40 +0200

    deps update

--- web/src/reportWebVitals.ts
+++ web/src/reportWebVitals.ts
@@ -1,12 +1,12 @@
-import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
+import { onCLS, onINP, onFCP, onLCP, onTTFB } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
     if (onPerfEntry && onPerfEntry instanceof Function) {
-        getCLS(onPerfEntry);
-        getFID(onPerfEntry);
-        getFCP(onPerfEntry);
-        getLCP(onPerfEntry);
-        getTTFB(onPerfEntry);
+        onCLS(onPerfEntry);
+        onINP(onPerfEntry);
+        onFCP(onPerfEntry);
+        onLCP(onPerfEntry);
+        onTTFB(onPerfEntry);
     }
 };
 
